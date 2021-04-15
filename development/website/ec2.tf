8c926360dc87ca0a168c74b9ca9f67b3fe0e3ba8
Eleatzar Colomer <ecolomer@abaenglish.com>
2021-04-15 12:55:05 +0200

    Add invalid EC2

--- development/website/ec2.tf
+++ development/website/ec2.tf
@@ -1,6 +1,6 @@
 ## Instance website ##
 resource "aws_instance" "ec-website" {
-  ami           = "ami-08bac620dc84221eb"
-  instance_type = "t2.nano" # invalid type!
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t1.2xlarge" # invalid type!
+  iam_instance_profile = "invalid-profile"
 }
-
